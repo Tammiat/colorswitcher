532986166c9c5b3d529c6375817fe44a47d08b9c
tamanate <tamanateaminata08@gmail.com>
2023-10-09 14:31:28 +0200

    new commit

---
+++ script.js
@@ -0,0 +1,6 @@
+let color ="yellow"
+function setBgColor(){
+    document.getElementById("bg").style.backgroundColor = color;
+    document.getElementById("bgcolor").innerHTML = color;
+    document.getElementById("bgcolor").style.color = color;
+}
